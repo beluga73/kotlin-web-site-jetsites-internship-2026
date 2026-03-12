f4f3b4c989100ce8f9e77e1bd4bf3a54a0d1c738
Mikhail <mikhailbelov737@gmail.com>
2026-03-12 08:44:56 +0100

    chore: move static files and layout

---
+++ migrate-rr7/app/components/layout.tsx
@@ -0,0 +1,22 @@
+export function Section({
+  children,
+  className,
+  id,
+}: {
+  children: React.ReactNode;
+  className?: string;
+  id?: string;
+}) {
+  return (
+    <section
+      id={id}
+      className={`kto-layout-section${className ? " " + className : ""}`}
+    >
+      {children}
+    </section>
+  );
+}
+
+export function Container({ children }: { children: React.ReactNode }) {
+  return <div className="kto-layout-container">{children}</div>;
+}
